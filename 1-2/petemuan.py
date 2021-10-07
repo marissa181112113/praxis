aef5609d52f05769dadedcc60c7b942a5053a47b
Marissa Azzahro <marissaazzahro68@gmail.com>
2021-10-07 15:17:04 +0700

    peerkenalan struktur data

---
+++ 1-2/petemuan.py
@@ -0,0 +1,31 @@
+#string
+a= "phyton pemula"
+print(type(a))
+#Menyatakan karakter /kalimat bisa berupa huruf, angka, dll dengan diapit " atau '
+
+
+#integer
+print (20)
+#Menyatakan bil bulat 
+
+#float
+print (3.14)
+#Menyatakan bil yang menyukai koma
+
+#list
+angka = [1,2,3,4,5]
+print (angka)
+print ('satu', 'dua', 'tiga')
+
+#tuple
+print ((1,2,3,4,5))
+print(("satu", "dua", "tiga"))
+
+#dictionary
+biodata ={
+    "Nama"      : "Marissa Azzahro",
+    "NIM"       : "181112113",
+    "Jurusan"   : "Teknik Komputer", 
+}
+print (biodata)
+print (type(biodata))
