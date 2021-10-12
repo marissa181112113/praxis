4cbe26673669921b861a2b73e4e1e02a737bf358
Marissa Azzahro <marissaazzahro68@gmail.com>
2021-10-12 17:14:11 +0700

    update ujian

--- 1-6/food/task/views.py
+++ 1-6/food/task/views.py
@@ -1,11 +1,12 @@
 from django.http import request
 from django.shortcuts import redirect, render
+from django.http import HttpResponse
 from . import models
 
 # Create your views here.
 def index (request) :
     if request.POST :
-        models.Task.objects.create(item=request.POST['item'])
+        models.Task.objects.create(item=request.POST['item'], jenis=request.POST['jenis'], jumlah=request.POST['jumlah'], harga=request.POST['harga'])
         return redirect('/')
     tasks = models.Task.objects.all()
     return render(request, 'index.html', {
